d4fb0c85e33006a904108a803d4ac9121c38c48e
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-26 13:11:10 +0400

    Add completed solution code for stage 4

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,24 +1,40 @@
 package cinema
 
 fun main() {
-    // write your code here
+
+    //region Room size input
     println("Enter the number of rows:")
     val rows: Int = readLine()!!.toInt()
     println("Enter the number of seats in each row: ")
     val seats: Int = readLine()!!.toInt()
+    //endregion
 
-    println()
-    println(Theatre.cinemaScheme(rows, seats))
-    println()
+    val theatre = Theatre()
 
-    println("Enter a row number:")
-    val rowNumber = readLine()!!.toInt()
-    println("Enter a seat number in that row:")
-    val seatInRow = readLine()!!.toInt()
+    do {
+        println()
+        println("1. Show the seats")
+        println("2. Buy a ticket")
+        println("0. Exit")
+        val menuChoice = readLine()!!.toInt()
+        println()
 
-    println()
-    print("Ticket price: ")
-    println("\$${Theatre.ticketPrice(rows, seats, rowNumber)}")
-    println()
-    println(Theatre.cinemaScheme(rows, seats, rowNumber, seatInRow))
+        when (menuChoice) {
+            1 -> {
+                println(theatre.cinemaScheme(rows, seats))
+            }
+            2 -> {
+                println("Enter a row number:")
+                val rowNumber = readLine()!!.toInt()
+                println("Enter a seat number in that row:")
+                val seatInRow = readLine()!!.toInt()
+
+                theatre.reserveSeat(rowNumber, seatInRow)
+
+                print("Ticket price: ")
+                println("\$${theatre.ticketPrice(rows, seats, rowNumber)}")
+            }
+            0 -> break
+        }
+    } while (menuChoice != 0)
 }
--- Cinema Room Manager/task/src/cinema/Theatre.kt
+++ Cinema Room Manager/task/src/cinema/Theatre.kt
@@ -2,31 +2,7 @@ package cinema
 
 import kotlin.math.floor
 
-object Theatre {
-
-    fun cinemaScheme(rows: Int, seats: Int): String {
-        val strBuilder: StringBuilder = StringBuilder()
-        strBuilder.append(CINEMA)
-        for (i in 0..rows) {
-            if (i == 0) {
-                strBuilder.appendLine()
-                strBuilder.append(" ")
-            } else {
-                strBuilder.appendLine()
-                strBuilder.append(i)
-            }
-            for (j in 0..seats) {
-                if (j == 0) {
-                    strBuilder.append(" ")
-                } else if (i == 0) {
-                    strBuilder.append("$j ")
-                } else {
-                    strBuilder.append("$SEAT ")
-                }
-            }
-        }
-        return strBuilder.toString()
-    }
+class Theatre {
 
     fun totalPriceSoldOut(rows: Int, seats: Int): Int {
         val isLargeRoom = rows * seats > LARGE_ROOM_MIN_SEATS
@@ -54,7 +30,11 @@ object Theatre {
         }
     }
 
-    fun cinemaScheme(rows: Int, seats: Int, rowNumber: Int, seatInRow: Int): String {
+    private val reservations: MutableList<Pair<Int, Int>> = mutableListOf()
+    fun reserveSeat(rowNumber: Int, seatInRow: Int) =
+        reservations.add(Pair(rowNumber, seatInRow))
+
+    fun cinemaScheme(rows: Int, seats: Int): String {
         val strBuilder: StringBuilder = StringBuilder()
         strBuilder.append(CINEMA)
         for (i in 0..rows) {
@@ -70,7 +50,9 @@ object Theatre {
                     strBuilder.append(" ")
                 } else if (i == 0) {
                     strBuilder.append("$j ")
-                } else if (i == rowNumber && j == seatInRow) {
+                } else if (reservations.any { (row, seat) ->
+                        row == i && seat == j
+                    }) {
                     strBuilder.append("$RESERVED_SEAT ")
                 } else {
                     strBuilder.append("$SEAT ")
@@ -79,11 +61,11 @@ object Theatre {
         }
         return strBuilder.toString()
     }
-
-    private const val SEAT = 'S'
-    private const val RESERVED_SEAT = 'B'
-    private const val CINEMA = "Cinema:"
-    private const val LARGE_ROOM_MIN_SEATS = 60
-    private const val TICKET_PRICE_10 = 10
-    private const val TICKET_PRICE_8 = 8
 }
+
+private const val SEAT = 'S'
+private const val RESERVED_SEAT = 'B'
+private const val CINEMA = "Cinema:"
+private const val LARGE_ROOM_MIN_SEATS = 60
+private const val TICKET_PRICE_10 = 10
+private const val TICKET_PRICE_8 = 8
